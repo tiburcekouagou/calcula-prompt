78282c264dc144a4c81310e5de1f9424016c11af
Oswald LEXPERT <oswald.adinavo@gmail.com>
2023-06-06 18:03:33 +0200

    ajout oswald

---
+++ exercice1.js
@@ -0,0 +1,13 @@
+/**
+ * Créer ici le projet « Calculaprompt ».
+ */
+
+const firstNumber = window.prompt("entrez un premier nombre");
+const operator = window.prompt("entrez un opérateur");
+const secondNumber = window.prompt("entrez un second nombre");
+
+let operation = (firstNumber + operator + secondNumber)
+let result = (firstNumber * secondNumber)
+
+console.log( operation );
+console.log( result );
