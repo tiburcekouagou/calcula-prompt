394b06306cfb096eacc003706905a1d48d74e77e
Singor Kpatchou <kpatchousingor1@gmail.com>
2023-06-06 17:32:41 +0100

    Projet calculator prompt

--- JS1.2.3-Calcula_Prompt_bonus2/corrigé/script.js
+++ JS1.2.3-Calcula_Prompt_bonus2/corrigé/script.js
@@ -3,22 +3,23 @@ const operat = window.prompt("Entrez l'opérateur ")
 const numb2 = window.prompt("Entrez le deuxième nombre ")
 
 
-
+if (operat === "+" || operat === "-" || operat === "*" || operat === "/") {
+    
     // Opératreur +
     if (operat === "+") {
-        if (isNaN(numb1) && isNaN(numb2)) {
+        if (isNaN(numb1) || isNaN(numb2)) {
             document.write("Entrez des chiffres s'il vous plait !!!");
         }
         else {
             const result = Number(numb1) + Number(numb2);
 
-            document.write(numb1 + operat + numb2 + "=" + result);
+            document.write( numb1 + operat + numb2 + "=" + result);
         }
     }
 
     // Opératreur -
     if (operat === "-") {
-        if (isNaN(numb1) && isNaN(numb2)) {
+        if (isNaN(numb1) || isNaN(numb2)) {
             document.write("Entrez des chiffres s'il vous plait !!!");
         }
         else {
@@ -31,7 +32,7 @@ const numb2 = window.prompt("Entrez le deuxième nombre ")
 
     // Opératreur *
     if (operat === "*") {
-        if (isNaN(numb1) && isNaN(numb2)) {
+        if (isNaN(numb1) || isNaN(numb2)) {
             document.write("Entrez des chiffres s'il vous plait !!!");
         }
         else {
@@ -44,7 +45,7 @@ const numb2 = window.prompt("Entrez le deuxième nombre ")
 
     // Opératreur /
     if (operat === "/") {
-        if (isNaN(numb1) && isNaN(numb2)) {
+        if (isNaN(numb1) || isNaN(numb2)) {
             document.write("Entrez des chiffres s'il vous plait !!!");
         }
         else {
@@ -53,3 +54,6 @@ const numb2 = window.prompt("Entrez le deuxième nombre ")
             document.write(numb1 + operat + numb2 + "=" + result);
         }
     }
+} else {
+    document.write("Entrez un opérateur valide s'il vous plait !!!");
+}
