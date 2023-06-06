724272b6a535c8df3c169cdfd303d6810e8df5aa
warrisagbannonde <awarrisw@gmail.com>
2023-06-06 18:14:31 +0200

    Commit Warris

--- exercice1.js
+++ exercice1.js
@@ -9,38 +9,42 @@
 
 
 /* Message de Bienvenue */
-window.alert("Bienvenue dans la calculatrice");
+window.alert(" 😉😉😉 Bienvenue dans la calculatrice 😉😉😉");
 
 /* Cette ligne demande à l'utilisateur d'entrer le premier nombre du calcul à effectuer puis ce nombre sera stockée dans la variable nombre_1 */
-const number_1 = Number(window.prompt("Entrez le premier nombre !"));
+const number_1 = Number(window.prompt(" ✏ Entrez le premier nombre ! ✏"));
 
 /* Cette ligne demande à l'utilisateur d'entrer l'opérateur arithmétique à utiliser our effectuer le calcul puis la valeur sera stockée dans la variable operateur */
-const operateur = window.prompt("Entrez un opérateur arithmétique");
+const operateur = window.prompt(" ✏ Entrez un opérateur arithmétique ✏");
 
 /* Cette ligne demande à l'utilisateur d'entrer le second nombre du calcul à effectuer puis ce nombre sera stockée dans la variable nombre_2 */
-const number_2 = Number(window.prompt("Entrez le second nombre !"));
+const number_2 = Number(window.prompt(" ✏ Entrez le second nombre ! ✏ "));
 
 /* Déclaration de la variable result qui va stocker le résultat du calcul*/
 let result;
 
+/* Condition à vérifiée avant d'effectuer l'opération*/
 if (operateur === "*") {
-    console.log(number_1 + " * " + number_2);
+    console.log("Calcul: " + number_1 + " * " + number_2);
     result = number_1 * number_2;
 } else if (operateur === "/"){
-    console.log(number_1 + " / " + number_2);
+    console.log("Calcul: " + number_1 + " / " + number_2);
     result = number_1 / number_2;
 }else if (operateur === "+"){
-    console.log(number_1 + " + " + number_2);
+    console.log("Calcul: " + number_1 + " + " + number_2);
     result = number_1 + number_2;
 }else if (operateur === "-"){
-    console.log(number_1 + " - " + number_2);
+    console.log("Calcul: " + number_1 + " - " + number_2);
     result = number_1 - number_2;
 }else {
     window.alert("Désolé vous ne pouvez pas effectuer une opération. 🙄")
 }
 
+/* Confirmation du calcul à effectuer */
+window.alert("Vous avez bien renseigné "  + number_1 + operateur + number_2);
+
 /* Affichage du résultat */
-console.log(result);
+console.log("Résultat: " + result);
 
 
 
