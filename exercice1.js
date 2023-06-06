ab7fb6b0d7d7529226f0524d86ecc28a79077e41
Oswald LEXPERT <oswald.adinavo@gmail.com>
2023-06-06 19:21:09 +0200

    calcula prompt

--- exercice1.js
+++ exercice1.js
@@ -3,7 +3,7 @@
  */
 
 const firstNumber = window.prompt("entrez un premier nombre");
-const operator = window.prompt("entrez un opérateur");
+const operator = window.prompt("entrez un opérateur + , - , * , /");
 const secondNumber = window.prompt("entrez un second nombre");
 
 let operation = (firstNumber + operator + secondNumber)
