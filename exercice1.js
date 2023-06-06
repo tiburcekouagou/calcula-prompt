7ee5f7f264a449a43b296f3ee0f33478e2723847
ATTOLOU <regisattolou21@outlook.fr>
2023-06-06 17:36:41 +0100

    Commit by Regis

--- exercice1.js
+++ exercice1.js
@@ -4,7 +4,7 @@
 
 const number1 = Number(window.prompt("Enter the number one:"));
 
-const operator = String(window.prompt("Enter the operator sign +, -, *, /, ÷:"));
+const operator = String(window.prompt("Enter the operator sign +, -, *, /:"));
 
 const number2 = Number(window.prompt("Enter the number two:"));
 
@@ -12,19 +12,23 @@ if (isNaN(number1) === false || isNaN(number2) === false) {
 
     if (operator === "+") {
         const result = number1 + number2;
-        alert(result);
-    } else if (operator === "-") {
+        alert("The answers of our operation"+ " " + number1 + " " + "+" + " " + number2 + " " + " is : "
+        + result);
+    } else if (operator === "-") { 
         const result = number1 - number2;
-        alert(result);
+        alert("The answers of our operation"+ " " + number1 + " " + "-" + " " + number2 + " " + " is : "
+        + result);
     } else if (operator === "*") {
         const result = number1 * number2;
-        alert(result);
+        alert("The answers of our operation"+ " " + number1 + " " + "*" + " " + number2 + " " + " is : "
+        + result);
     } else if (operator === "/") {
         const result = number1 / number2;
-        alert(result);
+        alert("The answers of our operation"+ " " + number1 + " " + "/" + " " + number2 + " " + " is : "
+        + result);
     }
 }else {
-    alert("Please enter a number");
+    alert("Please enter a number or maybe are you a crazy ?");
 }
 
 
